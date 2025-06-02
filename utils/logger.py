5f71029f58a236e5e0fb5b361757fb6ac47dd90a
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-02 01:26:42 +0100

    Added colour formatting to logs to better classify outputs.

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,30 @@
 import logging
 
 
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': '\033[94m',    # Blue
+        'INFO': '\033[92m',     # Green
+        'WARNING': '\033[93m',  # Yellow
+        'ERROR': '\033[91m',    # Red
+        'CRITICAL': '\033[95m', # Magenta
+    }
+    RESET = '\033[0m'
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelname, self.RESET)
+        message = super().format(record)
+        return f"{color}{message}{self.RESET}"
+
+
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
 
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter(
-            '[%(asctime)s -> %(levelname)s] %(name)s: %(message)s',
+        formatter = ColorFormatter(
+            '[%(name)s -> %(asctime)s] %(levelname)s: %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S'
         )
 
