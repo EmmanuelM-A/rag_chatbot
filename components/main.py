86685321341e6f71715cfbeb0e809d492dcd971f
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-06 01:48:47 +0100

    Created a terminal_usage method to run the script in terminal and have tested that full rag pipeline works.

--- components/main.py
+++ components/main.py
@@ -1,30 +1,68 @@
 from embedder import prepare_document_chunks, create_embedded_chunks
 from vector_store import load_faiss_index, save_faiss_index
-from query_handler import search
-from config import RAW_DOCS_DIRECTORY
+from query_handler import search, generate_response
+from config import RAW_DOCS_DIRECTORY, METADATA_PATH, INDEX_PATH
+import os
+from utils.logger import get_logger
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = get_logger("main_logger")
 
-def main():
-    # Step 1: Load and chunk
-    documents = prepare_document_chunks(RAW_DOCS_DIRECTORY)
 
-    # Step 2: Embed
-    vectors, metadata = create_embedded_chunks(documents)
+def terminal_usage(user_query):
+    """
+    Processes a user query by leveraging a RAG (Retrieval Augmented Generation) pipeline.
+    It checks for an existing vector store, builds it if necessary,
+    retrieves relevant document chunks, and generates an LLM-based response.
 
-    # Step 3: Save to disk
-    save_faiss_index(vectors, metadata)
+    Args:
+        user_query (str): The question or query provided by the user.
+    """
 
-    # Step 4: Load index and search
+    if os.path.exists(INDEX_PATH) and os.path.exists(METADATA_PATH):
+        logger.info("Existing FAISS index and metadata found. Loading now...")
+    else:
+        logger.info("FAISS index or metadata not found. Creating new ones.")
+
+        documents = prepare_document_chunks(RAW_DOCS_DIRECTORY)
+
+        vectors, metadata = create_embedded_chunks(documents)
+
+        save_faiss_index(vectors, metadata)
+
     index, meta = load_faiss_index()
 
-    results = search("What are my hobbies?", index, meta)
+    results = search(user_query, index, meta)
 
-    for r in results:
-        print(f"Found: {r['text']}\nFrom: {r['metadata']}\n")
+    if results:
+        llm_response_data = generate_response(user_query, results)
+        print(f"Response: {llm_response_data["answer"]}")
+    else:
+        print("\nNo relevant information found in the documents.")
+
+
+def main():
+    """
+    Main function to run the chatbot in a terminal interface.
+    Initializes the chatbot and enters a loop to accept user input.
+    """
+    print("Hi, I am Junie. Emmanuel's personal assistant chatbot. If you have any question about him, feel free to ask me.")
+
+    # Start an infinite loop to continuously accept user queries.
+    while True:
+        # Prompt the user for input.
+        query = input("")
+
+        # Check for a 'quit' command to exit the chatbot.
+        if query.lower() == "quit":
+            print("Happy to be of service! Goodbye for now.")
+            return
+
+        # Process the user's query
+        terminal_usage(query)
 
 
 if __name__ == "__main__":
