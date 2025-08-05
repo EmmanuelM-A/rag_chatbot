5177301e8761e5fde6e3eaa0144914619d470e6f
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-08-05 22:33:27 +0100

    implemented all document loader for the allowed file extensions

--- src/components/ingestion/document_loader.py
+++ src/components/ingestion/document_loader.py
@@ -1,10 +1,21 @@
 """
 Responsible for loading documents of different formats.
 """
-
+import os
 from abc import abstractmethod, ABC
 
-from src.components.ingestion.document import FileDocument
+import docx
+import fitz
+
+from src.components.ingestion.document import FileDocument, \
+    FileDocumentMetadata
+
+
+MD_FILE_EXT = ".md"
+TXT_FILE_EXT = ".txt"
+PDF_FILE_EXT = ".pdf"
+DOCX_FILE_EXT = ".docx"
+
 
 
 class DocumentLoader(ABC):
@@ -15,12 +26,11 @@ class DocumentLoader(ABC):
     """
 
     @abstractmethod
-    def load_data(self, source: str) -> FileDocument:
+    def load_data(self, file_path: str) -> FileDocument:
         """
-        Load data from the specified source.
+        Load data from the specified file path.
 
-        :param source: The source from which to load data (e.g.,
-        file path, URL).
+        :param file_path: The file path from which to load data (e.g., file path, URL).
         :return: The FileDocument representation of the file.
         """
 
@@ -40,8 +50,30 @@ class TxTDocumentLoader(DocumentLoader):
     Document loader responsible for loading txt files.
     """
 
-    def load_data(self, source: str) -> FileDocument:
-        pass
+    def load_data(self, file_path: str) -> FileDocument:
+        try:
+            try:
+                with open(file_path, "r", encoding="utf-8") as file:
+                    content = file.read()
+            except UnicodeDecodeError:
+                with open(file_path, 'r', encoding='latin-1') as file:
+                    content = file.read()
+
+            filename = os.path.splitext(file_path)[0].lower()
+
+            file_metadata = FileDocumentMetadata(
+                filename=filename,
+                file_extension=TXT_FILE_EXT,
+                author=None,
+                source=file_path
+            )
+
+            return FileDocument(
+                content=content,
+                metadata=file_metadata
+            )
+        except ValueError as e:
+            raise e
 
 
 class MarkdownDocumentLoader(DocumentLoader):
@@ -49,8 +81,30 @@ class MarkdownDocumentLoader(DocumentLoader):
     Document loader responsible for loading markdown files.
     """
 
-    def load_data(self, source: str) -> FileDocument:
-        pass
+    def load_data(self, file_path: str) -> FileDocument:
+        try:
+            try:
+                with open(file_path, "r", encoding="utf-8") as file:
+                    content = file.read()
+            except UnicodeDecodeError:
+                with open(file_path, 'r', encoding='latin-1') as file:
+                    content = file.read()
+
+            filename = os.path.splitext(file_path)[0].lower()
+
+            file_metadata = FileDocumentMetadata(
+                filename=filename,
+                file_extension=MD_FILE_EXT,
+                author=None,
+                source=file_path
+            )
+
+            return FileDocument(
+                content=content,
+                metadata=file_metadata
+            )
+        except ValueError as e:
+            raise e
 
 
 class PDFDocumentLoader(DocumentLoader):
@@ -58,8 +112,28 @@ class PDFDocumentLoader(DocumentLoader):
     Document loader responsible for loading pdf files.
     """
 
-    def load_data(self, source: str) -> FileDocument:
-        pass
+    def load_data(self, file_path: str) -> FileDocument:
+        try:
+            content = ""
+            with fitz.open(file_path) as doc:
+                for page in doc:
+                    content += page.get_text("text")
+
+            filename = os.path.splitext(file_path)[0].lower()
+
+            file_metadata = FileDocumentMetadata(
+                filename=filename,
+                file_extension=PDF_FILE_EXT,
+                author=None,
+                source=file_path
+            )
+
+            return FileDocument(
+                content=content,
+                metadata=file_metadata
+            )
+        except ValueError as e:
+            raise e
 
 
 class DocxDocumentLoader(DocumentLoader):
@@ -67,5 +141,25 @@ class DocxDocumentLoader(DocumentLoader):
     Document loader responsible for loading docx files.
     """
 
-    def load_data(self, source: str) -> FileDocument:
-        pass
+    def load_data(self, file_path: str) -> FileDocument:
+        try:
+            doc = docx.Document(str(file_path))
+            content = "\n".join(
+                [para.text for para in doc.paragraphs]
+            )
+
+            filename = os.path.splitext(file_path)[0].lower()
+
+            file_metadata = FileDocumentMetadata(
+                filename=filename,
+                file_extension=DOCX_FILE_EXT,
+                author=None,
+                source=file_path
+            )
+
+            return FileDocument(
+                content=content,
+                metadata=file_metadata
+            )
+        except ValueError as e:
+            raise e
